cc97efaf6e19b0529e069841343ce51fc5c02b6f
Peter Fogg <pfogg@edx.org>
2015-09-04 13:29:57 -0400

    Respond to AJAX errors in Teams UI.
    TNL-3042

--- lms/djangoapps/teams/static/teams/js/teams_tab_factory.js
+++ lms/djangoapps/teams/static/teams/js/teams_tab_factory.js
@@ -1,12 +1,30 @@
 ;(function (define) {
     'use strict';
-
-    define(['jquery', 'underscore', 'backbone', 'teams/js/views/teams_tab'],
-        function ($, _, Backbone, TeamsTabView) {
+    define([
+        'jquery',
+        'underscore',
+        'backbone',
+        'gettext',
+        'teams/js/views/teams_tab',
+        'teams/js/views/team_utils'
+    ], function ($, _, Backbone, gettext, TeamsTabView, TeamUtils) {
             return function (options) {
                 var teamsTab = new TeamsTabView({
                     el: $('.teams-content'),
                     context: options
+                });
+                $(document).ajaxError(function (event, xhr) {
+                    var message;
+                    if (xhr.status === 401) {
+                        message = gettext("We couldn't complete your request. Please reload the page and try again.");
+                    }
+                    else if (xhr.status === 500) {
+                        message = gettext("We had some trouble processing your request. Please try again.");
+                    }
+                    else {
+                        return;
+                    }
+                    TeamUtils.showMessage(message);
                 });
                 teamsTab.start();
             };
