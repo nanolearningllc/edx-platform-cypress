don-github/edx-platform

Feature flagged django sudo.

common/djangoapps/django_sudo_helpers/tests/test_startup.py
@@ -6,6 +6,7 @@ import ddt
 from django.conf import settings
 from django.test import TestCase
 from mock import patch
+
 from django_sudo_helpers.startup import run
 
 
@@ -18,12 +19,12 @@ class DjangoSudoStartupTestCase(TestCase):
         True,
         False,
     )
-    def test_run_third_party_auth(self, is_enable):
-        with patch.dict("django.conf.settings.FEATURES", {"ENABLE_THIRD_PARTY_AUTH": is_enable}):
-            self.assertEqual(settings.FEATURES["ENABLE_THIRD_PARTY_AUTH"], is_enable)
-            with patch('django_sudo_helpers.startup.enable_third_party_auth_for_sudo') as mock_third_party_for_sudo:
+    def test_run_django_sudo(self, is_enable):
+        with patch.dict("django.conf.settings.FEATURES", {"ENABLE_DJANGO_SUDO": is_enable}):
+            self.assertEqual(settings.FEATURES["ENABLE_DJANGO_SUDO"], is_enable)
+            with patch('django_sudo_helpers.startup.enable_django_sudo') as mock_enable_django_sudo:
                 run()
                 if is_enable:
-                    self.assertTrue(mock_third_party_for_sudo.called)
+                    self.assertTrue(mock_enable_django_sudo.called)
                 else:
-                    self.assertFalse(mock_third_party_for_sudo.called)
+                    self.assertFalse(mock_enable_django_sudo.called)

common/djangoapps/terrain/steps.py
@@ -15,6 +15,7 @@
 # so we import this as a module, and then read django_url from
 # it to get the correct value
 import lettuce.django
+from django.conf import settings
 
 from lettuce import world, step
 from .course_helpers import *
@@ -251,13 +252,13 @@ def i_get_sudo_access(_step, password):
     Note that wait_for empty is due to password field
     It will return password like this **** not text.
     """
-
-    sudo_form = world.css_find('form.sudo-form')
-    # check if sudo form is available then submit password to get sudo access
-    # otherwise return True because sudo access already given.
-    if len(sudo_form) > 0:
-        css_selector = 'input[id=id_password]'
-        world.retry_on_exception(lambda: world.css_find(css_selector)[0].fill(password))
-        world.wait_for(lambda _: not world.css_has_value(css_selector, '', index=0))
-        world.css_click('input[type=submit]')
-    return True
+    if settings.FEATURES.get('ENABLE_DJANGO_SUDO', False):
+        sudo_form = world.css_find('form.sudo-form')
+        # check if sudo form is available then submit password to get sudo access
+        # otherwise return True because sudo access already given.
+        if len(sudo_form) > 0:
+            css_selector = 'input[id=id_password]'
+            world.retry_on_exception(lambda: world.css_find(css_selector)[0].fill(password))
+            world.wait_for(lambda _: not world.css_has_value(css_selector, '', index=0))
+            world.css_click('input[type=submit]')
+        return True

nanolearningllc/edx-platform-cypress

common/djangoapps/django_sudo_helpers/tests/test_startup.py
@@ -6,6 +6,7 @@ import ddt
 from django.conf import settings
 from django.test import TestCase
 from mock import patch
+
 from django_sudo_helpers.startup import run
 
 
@@ -18,12 +19,12 @@ class DjangoSudoStartupTestCase(TestCase):
         True,
         False,
     )
-    def test_run_third_party_auth(self, is_enabled):
-        with patch.dict("django.conf.settings.FEATURES", {"ENABLE_THIRD_PARTY_AUTH": is_enabled}):
-            self.assertEqual(settings.FEATURES["ENABLE_THIRD_PARTY_AUTH"], is_enabled)
-            with patch('django_sudo_helpers.startup.enable_third_party_auth_for_sudo') as mock_third_party_for_sudo:
+    def test_run_django_sudo(self, is_enabled):
+        with patch.dict("django.conf.settings.FEATURES", {"ENABLE_DJANGO_SUDO": is_enabled}):
+            self.assertEqual(settings.FEATURES["ENABLE_DJANGO_SUDO"], is_enabled)
+            with patch('django_sudo_helpers.startup.enable_django_sudo') as mock_enable_django_sudo:
                 run()
                 if is_enabled:
-                    self.assertTrue(mock_third_party_for_sudo.called)
+                    self.assertTrue(mock_enable_django_sudo.called)
                 else:
-                    self.assertFalse(mock_third_party_for_sudo.called)
+                    self.assertFalse(mock_enable_django_sudo.called)

common/djangoapps/terrain/steps.py
@@ -15,6 +15,7 @@
 # so we import this as a module, and then read django_url from
 # it to get the correct value
 import lettuce.django
+from django.conf import settings
 
 from lettuce import world, step
 from .course_helpers import *
@@ -251,13 +252,13 @@ def i_get_sudo_access(_step, password):
     Note that wait_for empty is due to password field
     It will return password like this **** not text.
     """
-
-    sudo_form = world.css_find('form.sudo-form')
-    # check if sudo form is available then submit password to get sudo access
-    # otherwise return True because sudo access already given.
-    if len(sudo_form) > 0:
-        css_selector = 'input[id=id_password]'
-        world.retry_on_exception(lambda: world.css_find(css_selector)[0].fill(password))
-        world.wait_for(lambda _: not world.css_has_value(css_selector, '', index=0))
-        world.css_click('button[type=submit]')
-    return True
+    if settings.FEATURES.get('ENABLE_DJANGO_SUDO', False):
+        sudo_form = world.css_find('form.sudo-form')
+        # check if sudo form is available then submit password to get sudo access
+        # otherwise return True because sudo access already given.
+        if len(sudo_form) > 0:
+            css_selector = 'input[id=id_password]'
+            world.retry_on_exception(lambda: world.css_find(css_selector)[0].fill(password))
+            world.wait_for(lambda _: not world.css_has_value(css_selector, '', index=0))
+            world.css_click('button[type=submit]')
+        return True